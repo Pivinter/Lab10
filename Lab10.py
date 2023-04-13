528d2fffbca69245931b44b8088055eb9af4bfda
Vladuslav Kuliavets <70973362+Pivinter@users.noreply.github.com>
2023-04-13 23:34:19 +0300

    Add files via upload

--- Lab10.py
+++ Lab10.py
@@ -2,124 +2,131 @@ import random
 
 class Node:
     def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
-        self.value = value
 
-def read_file(filename):
-    with open(filename, 'r') as file:
-        data = file.read().splitlines()
-    return [int(value) for value in data]
-
-def build_tree(values):
-    root = Node(values[0])
-    for value in values[1:]:
-        insert_node(root, Node(value))
-    return root
-
-def insert_node(root, node):
+def add_node(root, value):
     if root is None:
-        root = node
-    elif node.value < root.value:
-        if root.left is None:
-            root.left = node
-        else:
-            insert_node(root.left, node)
+        return Node(value)
     else:
-        if root.right is None:
-            root.right = node
+        if value < root.value:
+            root.left = add_node(root.left, value)
         else:
-            insert_node(root.right, node)
-
-def swap_children(node):
-    if node is None:
-        return
-    if node.left is not None and node.right is not None:
-        node.left, node.right = node.right, node.left
-        swap_children(node.left)
-        swap_children(node.right)
-    elif node.left is not None:
-        node.right = Node(random.randint(0, 100))
-        node.left, node.right = node.right, node.left
-        swap_children(node.left)
-        swap_children(node.right)
-    elif node.right is not None:
-        node.left = Node(random.randint(0, 100))
-        node.left, node.right = node.right, node.left
-        swap_children(node.left)
-        swap_children(node.right)
+            root.right = add_node(root.right, value)
+    return root
 
 def find_node(root, value):
     if root is None or root.value == value:
         return root
-    elif value < root.value:
+
+    if value < root.value:
         return find_node(root.left, value)
     else:
         return find_node(root.right, value)
 
-def delete_node(root, value):
+def remove_node(root, value):
     if root is None:
         return root
+
     if value < root.value:
-        root.left = delete_node(root.left, value)
-    elif value > root.value:
-        root.right = delete_node(root.right, value)
+        root.left = remove_node(root.left, value)
     else:
-        if root.left is None:
-            temp = root.right
-            root = None
-            return temp
-        elif root.right is None:
-            temp = root.left
-            root = None
-            return temp
-        temp = find_min_node(root.right)
-        root.value = temp.value
-        root.right = delete_node(root.right, temp.value)
+        if value > root.value:
+            root.right = remove_node(root.right, value)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            min_larger_node = find_min(root.right)
+            root.value = min_larger_node.value
+            root.right = remove_node(root.right, min_larger_node.value)
+
     return root
 
-def find_min_node(node):
+def find_min(node):
     current = node
     while current.left is not None:
         current = current.left
     return current
 
-def print_tree(root):
-    if root is not None:
-        print_tree(root.left)
-        print(root.value)
-        print_tree(root.right)
+def build_tree_from_file(filename):
+    with open(filename, 'r') as file:
+        values = [int(value.strip()) for value in file.readlines()]
+    root = None
+    for value in values:
+        root = add_node(root, value)
+    return root
 
-def menu():
-    values = read_file('tree_values.txt')
-    root = build_tree(values)
+def swap_children(node):
+    if node is None:
+        return
 
-    while True:
-        print("Choose an option:")
-        print("1. Add a node")
-        print("2. Delete a node")
-        print("3. Search for a node")
-        print("4. Swap nodes")
-        print("5. Print the tree")
-        choice = input("Enter your choice: ")
+    if node.left is not None and node.right is not None:
+        node.left.value, node.right.value = node.right.value, node.left.value
 
-        if choice == "1":
-            value = int(input("Enter a value to add: "))
-            insert_node(root, Node(value))
-        elif choice == "2":
-            value = int(input("Enter a value to delete: "))
-            root = delete_node(root, value)
-        elif choice == "3":
-            value = int(input("Enter a value to search: "))
+    swap_children(node.left)
+    swap_children(node.right)
+
+def add_random_child(node):
+    if node is None:
+        return
+
+    if node.left is None and node.right is not None:
+        node.left = Node(random.randint(1, 100))
+    elif node.left is not None and node.right is None:
+        node.right = Node(random.randint(1, 100))
+
+    add_random_child(node.left)
+    add_random_child(node.right)
+
+def print_tree(node, indent=0):
+    if node is not None:
+        print_tree(node.left, indent + 4)
+        print(" " * indent + str(node.value))
+        print_tree(node.right, indent + 4)
+
+
+
+root = build_tree_from_file('tree_values.txt')
+swap_children(root)
+add_random_child(root)
+print("Оберіть операцію:")
+print("1. Додати вузол")
+print("2. Видалити вузол")
+print("3. Знайти вузол")
+print("4. Показати дерево")
+print("5. Обміняти вузли")
+print("6. Завершити програму")
+
+while True:
+        choice = input("Введіть номер операції: ")
+
+        if choice == '1':
+            value = int(input("Введіть значення вузла для додавання: "))
+            root = add_node(root, value)
+            print("Вузол успішно додано.")
+        elif choice == '2':
+            value = int(input("Введіть значення вузла для видалення: "))
+            root = remove_node(root, value)
+            print("Вузол успішно видалено.")
+        elif choice == '3':
+            value = int(input("Введіть значення вузла для пошуку: "))
             node = find_node(root, value)
             if node is None:
-                print("Node not found")
+                print("Вузол не знайдено.")
             else:
-                print("Node found")
-        elif choice == "4":
-            swap_children(root)
-        elif choice == "5":
+                print("Вузол знайдено.")
+        elif choice == '4':
+            print("Бінарне дерево:")
             print_tree(root)
-
-if __name__ == '__main__':
-    menu()
+        elif choice == '5':
+            swap_children(root)
+            print("Вузли успішно обмінено.")
+        elif choice == '6':
+            print("Завершення програми.")
+            break
+        else:
+            print("Невірний вибір. Спробуйте ще раз.")
--- import unittest.py
+++ import unittest.py
@@ -1,31 +1,38 @@
 import unittest
-from Lab10 import Node, find_node
+import random
+
+from Lab10 import *
+
 
 class TestBinaryTree(unittest.TestCase):
+    def setUp(self):
+        self.root = Node(5)
+        self.root.left = Node(3)
+        self.root.right = Node(7)
+        self.root.left.left = Node(2)
+        self.root.left.right = Node(4)
+        self.root.right.left = Node(6)
+        self.root.right.right = Node(8)
+
+    def test_add_node(self):
+        add_node(self.root, 1)
+        self.assertIsNotNone(self.root.left.left.left)
 
     def test_find_node(self):
-        root = Node(5)
-        node2 = Node(2)
-        node7 = Node(7)
-        node1 = Node(1)
-        node3 = Node(3)
-        node6 = Node(6)
-        node8 = Node(8)
+        found_node = find_node(self.root, 4)
+        self.assertIsNotNone(found_node)
+        self.assertEqual(found_node.value, 4)
 
-        root.left = node2
-        root.right = node7
-        node2.left = node1
-        node2.right = node3
-        node7.left = node6
-        node7.right = node8
 
-        self.assertEqual(find_node(root, 5), root)
-        self.assertEqual(find_node(root, 2), node2)
-        self.assertEqual(find_node(root, 7), node7)
-        self.assertEqual(find_node(root, 1), node1)
-        self.assertEqual(find_node(root, 3), node3)
-        self.assertEqual(find_node(root, 6), node6)
-        self.assertEqual(find_node(root, 8), node8)
-        
+    def test_find_min(self):
+        min_node = find_min(self.root.right)
+        self.assertEqual(min_node.value, 6)
+
+    def test_swap_children(self):
+        swap_children(self.root)
+        self.assertEqual(self.root.left.value, 7)
+        self.assertEqual(self.root.right.value, 3)
+
+
 if __name__ == '__main__':
     unittest.main()
